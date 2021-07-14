c9dc1296825d7a05f0965a84ed9b86f537b4e258
Aaliyah Jardien <aaliyahjar13@gmail.com>
2021-07-14 23:01:43 +0200

    changes

--- Practice-Exercises/app3.py
+++ Practice-Exercises/app3.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template
 # simple html template
 app = Flask(__name__)
 
-@app.route('/<name>')
+@app.route('/<name>', methods=['GET', 'POST'])
 def home(name):
     return render_template('homepage.html', name=name)
 
 @app.route('/looping/<int:number>')
-def looping(numbers)
+def looping(number):
     return render_template('loopy.html', number=number)
+
+@app.route('/image', methods=['GET', 'POST'])
+def image():
+    return render_template('image.html')
 
 if __name__ == '__main__':
     app.debug = True
